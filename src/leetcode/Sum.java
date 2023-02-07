5fb4503243d5faa7ad7a052437c0a911b1254485
hasan <hasaniips42@gmail.com>
2023-02-07 12:02:39 +0530

    second commit

---
+++ src/leetcode/Sum.java
@@ -0,0 +1,17 @@
+package leetcode;
+import java.util.*;
+public class Sum {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the no");
+		int x=sc.nextInt();
+		System.out.println("enter the no");
+		int y=sc.nextInt();
+		System.out.println(x+y);
+		
+		
+
+	}
+
+}
